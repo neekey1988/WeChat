317174722535db246147e19bc052bd5b6bf6f8af
任晨光 <renchenguang@sheca.com>
2020-12-10 15:36:29 +0800

    all

---
+++ WeChat/build.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+
+#docker中执行此脚本进行代码发布构建
+solutionName=$1 #解决方案名称
+containerName=$2 #容器名称
+
+docker stop $containerName
+docker rm $containerName
+docker rmi $containerName
+docker build -t $containerName /var/publish/$solutionName/
+echo "镜像构建成功"
